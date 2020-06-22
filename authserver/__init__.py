2fca745f4b7f1c5c633ff3005b5888b91f377c51
kshitijmehta <kshittijmehta@gmail.com>
2020-06-23 00:13:21 +0530

    removed secrets

--- authserver/__init__.py
+++ authserver/__init__.py
@@ -2,10 +2,10 @@ from flask import Flask
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
+import secrets
 
 app = Flask(__name__)
-# app.config['SECRET_KEY'] = 'auth_secret'
-app.config['JWT_SECRET_KEY'] = '$29000$F.K8lxLi3HuP0bo3Rui9Vw$dZy1iHEJZKJ/MgUAVPm1jaR9y/fK/0Xvt3AyRj3HvII'
+app.config['JWT_SECRET_KEY'] = secrets.secrets['JWT_SECRET_KEY']
 
 
 api = Api(app)
---
+++ secrets.example.py
@@ -0,0 +1,8 @@
+"""Use this structure to create secrets.py"""
+secrets = {
+    'JWT_SECRET_KEY': '',
+    'DB_NAME': '',
+    'DB_USER': '',
+    'DB_HOST': '',
+    'DB_PASSWORD': ''
+}
